589d6947ae766a86f163b63cd80827a7655421d3
Peter Ridolfi <petey.ridolfi7@gmail.com>
2022-05-31 15:19:13 -0400

    modifications

---
+++ import json.py
@@ -0,0 +1,12 @@
+import json
+import pyhf
+import numpy as np
+
+with open("C:/Users/peter/iris_project/shape_example_ws.json") as serialized:
+    spec = json.load(serialized)
+workspace = pyhf.Workspace(spec)
+model = workspace.model()
+observations = workspace.data(model, include_auxdata=True)
+poi_values = np.linspace(0, 5, 10)
+obs_limit, exp_limits, (scan, results) = pyhf.infer.intervals.upperlimit(observations, model, poi_values, level = 0.05, return_results= True)
+print(obs_limit, exp_limits)
--- pyhf_convert_to_datacard.py
+++ pyhf_convert_to_datacard.py
@@ -11,14 +11,12 @@ with open("C:/Users/peter/iris_project/shape_example_ws.json") as serialized:
 workspace = pyhf.Workspace(spec)
 sig = spec['channels'][0]['samples'][0]['data']
 bkg = spec['channels'][0]['samples'][1]['data']
-bkg_shapesys = spec['channels'][0]['samples'][1]['modifiers'][0]['data']
 bkg_histosys_up = spec['channels'][0]['samples'][1]['modifiers'][1]['data']['hi_data']
 bkg_histosys_down = spec['channels'][0]['samples'][1]['modifiers'][1]['data']['lo_data']
 observation = spec['observations'][0]['data']
 with uproot.recreate("C:/Users/peter/iris_project/example_shapes.root") as file:
     file["bin1/background"] = np.histogram(range(3), weights=bkg, bins = range(4))
     file["bin1/signal"] = np.histogram(range(3), weights=sig, bins = range(4))
-    file["bin1/background_uncorr"] = np.histogram(range(3), weights=bkg_shapesys, bins = range(4))
     file["bin1/background_alphaUp"] = np.histogram(range(3), weights=bkg_histosys_up, bins = range(4))
     file["bin1/background_alphaDown"] = np.histogram(range(3), weights=bkg_histosys_down, bins = range(4))
     file["bin1/data_obs"] = np.histogram(range(3), weights=observation, bins = range(4))
