828a09f88273dee450f30e449fd39d0f925fa730
Peter Ridolfi <petey.ridolfi7@gmail.com>
2022-06-07 18:33:59 +0000

    first converter

--- pyhf_converted_from_datacard.py
+++ pyhf_converted_from_datacard.py
@@ -1,256 +1,83 @@
-import pyhf
-import numpy as np
+import CMSSW_11_2_0.pyhf as pyhf
+
 import json
+import CMSSW_11_2_0.hist as hist
+import CMSSW_11_2_0.python.HiggsAnalysis.CombinedLimit.DatacardParser as DP
+
+from sys import exit
+from optparse import OptionParser
+
+from CMSSW_11_2_0.python.HiggsAnalysis.CombinedLimit.Datacard import Datacard
+parser = OptionParser()
+DP.addDatacardParserOptions(parser)
+options,args = parser.parse_args()
+
+dc_file=  open("/home/cmsusr/pyhf_converter/CMSSW_11_2_0/src/HiggsAnalysis/CombinedLimit/data/tutorials/counting/realistic-multi-channel.txt")
+DC = Datacard()
+DC = DP.parseCard(file=dc_file, options=options)
+dc_file.close()
+
+##get arrays of datacard info
+channels = [channel for channel in DC.bins]
+observations = [obs for channel, obs in DC.obs.items()]
+samples = [sample for sample in DC.processes]
+exp_values = DC.exp
+sig = DC.isSignal
+mods = DC.systs
+
 
 spec = {
 
   "channels": [
-    {
-      "name": "e_tau",
-      "samples": [
-        {
-          "name": "higgs",
-          "data": [
-            0.34
-          ],
-          "modifiers": [
-            {
-              "name": "mu",
-              "type": "normfactor",
-              "data": None
-            },
-            {
-              "name": "lumi",
-              "type": "normsys",
-              "data": {"hi": 1.11, "lo": 0.91}
-            },
-            {
-              "name": "tauid",
-              "type": "normsys",
-              "data": {"hi": 1.23, "lo": 0.81}
-            },
-            {
-              "name": "effic",
-              "type": "normsys",
-              "data": {"hi": 1.04, "lo": 0.96}
-            }
-          ]
-        },
-        {
-          "name": "zTT",
-          "data": [
-            190
-          ],
-          "modifiers": [
-            {
-              "name": "tauid",
-              "type": "normsys",
-              "data": {"hi": 1.23, "lo": 0.81}
-            },
-            {
-              "name": "ztoll",
-              "type": "normsys",
-              "data": {"hi": 1.04, "lo": 0.96}
-            },
-            {
-              "name": "effic",
-              "type": "normsys",
-              "data": {"hi": 1.04, "lo": 0.96}
-            }
-          ]
-        },
-        {
-             "name": "qcd",
-          "data": [
-            327
-          ],
-          "modifiers": [
-            {
-              "name": "qcdel",
-              "type": "normsys",
-              "data": {"hi": 1.20, "lo": 0.83}
-            }
-          ]
-        }        
-      ]
-    },
-    {
-      "name": "mu_tau",
-      "samples": [
-        {
-          "name": "higgs",
-          "data": [
-            0.57
-          ],
-          "modifiers": [
-            {
-              "name": "mu",
-              "type": "normfactor",
-              "data": None
-            },
-            {
-              "name": "lumi",
-              "type": "normsys",
-              "data": {"hi": 1.11, "lo": 0.91}
-            },
-            {
-              "name": "tauid",
-              "type": "normsys",
-              "data": {"hi": 1.23, "lo": 0.81}
-            },
-            {
-              "name": "effic",
-              "type": "normsys",
-              "data": {"hi": 1.04, "lo": 0.96}
-            }
-          ]
-        },
-        {
-          "name": "zTT",
-          "data": [
-            329
-          ],
-          "modifiers": [
-            {
-              "name": "tauid",
-              "type": "normsys",
-              "data": {"hi": 1.23, "lo": 0.81}
-            },
-            {
-              "name": "ztoll",
-              "type": "normsys",
-              "data": {"hi": 1.04, "lo": 0.96}
-            },
-            {
-              "name": "effic",
-              "type": "normsys",
-              "data": {"hi": 1.04, "lo": 0.96}
-            }
-          ]
-        },
-        {
-             "name": "qcd",
-          "data": [
-            259
-          ],
-          "modifiers": [
-            {
-              "name": "qcdmu",
-              "type": "normsys",
-              "data": {"hi": 1.10, "lo": 0.91}
-            }
-          ]
-        }        
-      ]
-    },
-    {
-      "name": "e_mu",
-      "samples": [
-        {
-          "name": "higgs",
-          "data": [
-            0.15
-          ],
-          "modifiers": [
-            {
-              "name": "mu",
-              "type": "normfactor",
-              "data": None
-            },
-            {
-              "name": "lumi",
-              "type": "normsys",
-              "data": {"hi": 0.91, "lo": 1.1}
-            },
-            {
-              "name": "effic",
-              "type": "normsys",
-              "data": {"hi": 1.04, "lo": 0.96}
-            }
-          ]
-        },
-        {
-          "name": "zTT",
-          "data": [
-            88
-          ],
-          "modifiers": [
-            {
-              "name": "ztoll",
-              "type": "normsys",
-              "data": {"hi": 1.04, "lo": 0.96}
-            },
-            {
-              "name": "effic",
-              "type": "normsys",
-              "data": {"hi": 1.04, "lo": 0.96}
-            }
-          ]
-        },
-        {
-             "name": "other",
-          "data": [
-            14
-          ],
-          "modifiers": [
-            {
-              "name": "lumi",
-              "type": "normsys",
-              "data": {"hi": 0.91, "lo": 1.1}
-            },
-            {
-              "name": "effic",
-              "type": "normsys",
-              "data": {"hi": 1.04, "lo": 0.96}
-            },
-            {
-              "name": "other",
-              "type": "normsys",
-              "data": {"hi": 1.1, "lo": 0.91}
-            }
-          ]
-        }        
-      ]
-    }
-
     
   ],
   "observations": [
-    {
-      "name": "e_tau",
-      "data": [
-            517
-      ]
-    },
-    {
-        "name":"mu_tau",
-        "data":[
-            540
-        ]
-    },
-    {
-        "name":"e_mu",
-        "data":[
-            101
-        ]
-    }
+    
   ],
   "measurements": [
-    {
-      "name": "Measurement",
-      "config": {
-        "poi": "mu",
-        "parameters": []
-      }
-    }
+  
   ],
   "version": "1.0.0"
 }
-workspace = pyhf.Workspace(spec)
-model = workspace.model()
 
-observations = workspace.data(model, include_auxdata = True)
-fit = pyhf.infer.mle.fit(data = observations, pdf = model)
-print (fit)
+for idxc, channel in enumerate(channels):
+  spec["channels"].append({"name": channel, "samples": []})
+  spec["observations"].append({"name": channel, "data": observations[idxc]})
+  for idxs, sample in enumerate(samples):
+    if sample in exp_values[channel].keys():
+      spec["channels"][idxc]["samples"].append({"name": sample, "data": exp_values[channel][sample], "modifiers": []})    
+for idx, sample in enumerate(samples):
+  if sig[sample]==True:
+    spec["measurements"].append({"name": "Measurement", "config": {"poi": "mu", "parameters": []}})  
+    
+for idxc, channel in enumerate(channels):
+  for idxs, sample in enumerate(samples):
+    if sig[sample] == True:
+      spec["channels"][idxc]["samples"][idxs]["modifiers"].append({"name": "mu", "type": "normfactor", "data": None})    
+for syst in mods:
+  name = syst[0]
+  mod_type = syst[2]
+  if mod_type == "lnN":
+    for idxc, channel in enumerate(channels):
+      for idxs, sample in enumerate(exp_values[channel].keys()):
+        if sample in exp_values[channel].keys():
+          if syst[4][channel][sample]!=0:
+            spec["channels"][idxc]["samples"][idxs]["modifiers"].append({"name": name, "type": mod_type, "data": [syst[4][channel][sample], 1/(syst[4][channel][sample])]})
+
+
+
+
+
+
+
+print(json.dumps(spec, indent=2))
+
+
+
+
+
+
+
+
 
 
