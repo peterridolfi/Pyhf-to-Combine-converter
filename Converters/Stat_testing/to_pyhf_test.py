44788946d6f7219d1ca4595ec60447bf6fffce1d
Peter Ridolfi <petey.ridolfi7@gmail.com>
2022-06-08 17:42:13 +0000

    organization

--- Converters/Stat_testing/to_pyhf_test.py
+++ Converters/Stat_testing/to_pyhf_test.py
@@ -9,7 +9,13 @@ with open("converted_workspace.json") as file:
 ws = pyhf.Workspace(spec)
 model = ws.model()
 observations = ws.data(model)
-poi_values = np.linspace(1, 10, 1)
+print(pyhf.infer.hypotest(
+    0.5, 
+    observations,
+    model,
+    test_stat = "qtilde"
+))
+poi_values = np.linspace(0, 10)
 obs_limit, exp_limits, (scan, results) = pyhf.infer.intervals.upperlimit(
     observations, model, poi_values, level=0.05, return_results=True
 )
