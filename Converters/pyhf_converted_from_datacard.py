5a53e49b0d6961028c5d3b7ec96896fa7350f97e
Peter Ridolfi <petey.ridolfi7@gmail.com>
2022-06-09 17:25:26 +0000

    shape analysis

--- Converters/pyhf_converted_from_datacard.py
+++ Converters/pyhf_converted_from_datacard.py
@@ -1,6 +1,8 @@
+from array import array
 from email.policy import default
 from modulefinder import Module
 import string
+from hist import Hist
 import pyhf
 import uproot
 import json
@@ -32,29 +34,99 @@ samples = [sample for sample in DC.processes]
 exp_values = DC.exp
 sig = DC.isSignal
 mods = DC.systs
+
    
 
+def getShapeFile(shapeMap: dict, channel, sample)->string:
+    file = ''
+    if not channel in shapeMap.keys():
+        if '*' in shapeMap.keys():
+            if not sample in shapeMap['*']:
+                if '*' in shapeMap['*'].keys():
+                    file = shapeMap["*"]["*"][0]
+            else:
+                file = shapeMap["*"][sample][0]
+    else:
+        file = shapeMap[channel][sample][0]
+    return file
 
+def getHistPath(shapeMap: dict, channel, sample)->string:
+    path = ''
+    if not channel in shapeMap.keys():
+        if '*' in shapeMap.keys():
+            if not sample in shapeMap['*']:
+                if '*' in shapeMap['*'].keys():
+                    path = shapeMap["*"]["*"][1].replace("$CHANNEL", channel).replace("$PROCESS", sample)
+            else:
+                path = shapeMap["*"][sample][1].replace("$CHANNEL", channel)
+    else:
+        path = shapeMap[channel][sample][1]
+    return path
 
+def getUncertPath(shapeMap: dict, channel, sample, name)->string:
+    path = ''
+    if not channel in shapeMap.keys():
+        if '*' in shapeMap.keys():
+            if not sample in shapeMap['*']:
+                if '*' in shapeMap['*'].keys():
+                    path = shapeMap["*"]["*"][2].replace("$CHANNEL", channel).replace("$PROCESS", sample).replace("$SYSTEMATIC", name)
+            else:
+                path = shapeMap["*"][sample][2].replace("$CHANNEL", channel).replace("SYSTEMATIC", name)
+    else:
+        path = shapeMap[channel][sample][2].replace("$SYSTEMATIC", name)
+    return path
+
+def getHist(shapeMap: dict, channel, sample):
+    file = uproot.open(getShapeFile(shapeMap, channel, sample))
+    hist = file[getHistPath(shapeMap, channel, sample)]
+    return hist
+def getUncertUp(shapeMap: dict, channel, sample, name):
+    file = uproot.open(getShapeFile(shapeMap, channel, sample))
+    hist = file[getUncertPath(shapeMap, channel, sample, name)+"Up"]
+    return hist
+def getUncertDown(shapeMap: dict, channel, sample, name):
+    file = uproot.open(getShapeFile(shapeMap, channel, sample))
+    hist = file[getUncertPath(shapeMap, channel, sample, name)+"Down"]
+    return hist
 
 
 def addChannels(spec: dict):
-    for idxc, channel in enumerate(channels):
-        spec["channels"].append({"name": channel, "samples": []})
-        spec["observations"].append({"name": channel, "data": [observations[idxc]]})
-
+    if DC.hasShapes:
+        for idxc, channel in enumerate(channels):
+            spec["channels"].append({"name": channel, "samples": []})
+            hist = getHist(DC.shapeMap, channel, "data_obs")
+            data = hist.to_numpy()[0].tolist()
+            spec["observations"].append({"name": channel, "data": data})
+    else:
+        for idxc, channel in enumerate(channels):
+            spec["channels"].append({"name": channel, "samples": []})
+            spec["observations"].append({"name": channel, "data": [observations[idxc]]})
 
 def addSamples(spec: dict):
-    for idxc, channel in enumerate(channels):
-        for idxs, sample in enumerate(samples):
-            if sample in exp_values[channel].keys():
-                spec["channels"][idxc]["samples"].append(
+    if DC.hasShapes:
+        for idxc, channel in enumerate(channels):
+            for idxs, sample in enumerate(samples):
+                hist = getHist(DC.shapeMap, channel, sample)
+                data = hist.to_numpy()[0].tolist()
+                if sample in exp_values[channel].keys():
+                    spec["channels"][idxc]["samples"].append(
                     {
                         "name": sample,
-                        "data": [exp_values[channel][sample]],
+                        "data": data,
                         "modifiers": [],
                     }
                 )
+    else:    
+        for idxc, channel in enumerate(channels):
+            for idxs, sample in enumerate(samples):
+                if sample in exp_values[channel].keys():
+                    spec["channels"][idxc]["samples"].append(
+                        {
+                            "name": sample,
+                            "data": [exp_values[channel][sample]],
+                            "modifiers": [],
+                        }
+                    )
 
 
 def addMeasurements(spec: dict):
@@ -93,6 +165,23 @@ def addMods(spec: dict):
                                     },
                                 }
                             )
+        if "shape" in mod_type:
+            for idxc, channel in enumerate(channels):
+                for idxs, sample in enumerate(samples):
+                    if syst[4][channel][sample] != 0:    
+                        if sample in exp_values[channel].keys():    
+                            histUp = getUncertUp(DC.shapeMap, channel, sample, name)
+                            histDown = getUncertDown(DC.shapeMap, channel, sample, name)
+                            hi_data = histUp.to_numpy()[0].tolist()
+                            lo_data = histDown.to_numpy()[0].tolist()
+                            spec["channels"][idxc]["samples"][idxs]["modifiers"].append(
+                            {
+                                "name": name,
+                                "type": "histosys",
+                                "data": {"hi_data": [syst[4][channel][sample]*count for count in hi_data], "lo_data": [syst[4][channel][sample]*count for count in lo_data]},
+                            }
+                        )
+
 
 
 def toJSON(spec: dict):
@@ -101,19 +190,16 @@ def toJSON(spec: dict):
     addMeasurements(spec)
     addNormFactor(spec)
     addMods(spec)
-    
-        
-spec = {"channels": [], "observations": [], "measurements": [], "version": "1.0.0"}
-
 
 def writeFileName(name):
     with open(name, "w") as file:
         file.write(json.dumps(spec, indent=2))
-
-
-
+    
+        
+spec = {"channels": [], "observations": [], "measurements": [], "version": "1.0.0"}
 toJSON(spec)
 writeFileName(options.outfile)
+
 
 
 
