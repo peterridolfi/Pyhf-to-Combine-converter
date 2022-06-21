6a666a0ea771dea02ae8797a63161122ad7c10e0
Peter Ridolfi <petey.ridolfi7@gmail.com>
2022-06-21 16:03:31 +0000

    black changes

--- Converters/pyhf_convert_to_datacard.py
+++ Converters/pyhf_convert_to_datacard.py
@@ -18,8 +18,10 @@ from HiggsAnalysis.CombinedLimit.Datacard import Datacard
 parser = OptionParser()
 parser.add_option("-o", "--out-file", dest="outfile", default="converted_datacard.txt")
 options, args = parser.parse_args()
-parser.add_option('-o', '--out-datacard', dest = "outdatacard", default = "converted_datacard.txt")
-parser.add_option('-s', 'shape-file', dest = 'shapefile', default = 'shapes.root')
+parser.add_option(
+    "-o", "--out-datacard", dest="outdatacard", default="converted_datacard.txt"
+)
+parser.add_option("-s", "shape-file", dest="shapefile", default="shapes.root")
 
 with open(args[0]) as serialized:
     spec = json.load(serialized)
@@ -37,18 +39,26 @@ DC.processes = samples
 
 uproot.recreate(options.shapefile)
 
+
 def addChannels():
     DC.bins = channels
     for idxc, channel in enumerate(channel_bins.keys()):
-        if channel_bins[channel] == 1: ##single bin
+        if channel_bins[channel] == 1:  ##single bin
             DC.obs.update({channel: workspace.data(model, include_auxdata=False)[idxc]})
         else:
             DC.obs.update({channel: -1})
             with uproot.update(options.shapefile) as file:
-                            h_data = hist.Hist.new.Regular(channel_bins[channel], 0, channel_bins[channel]).Weight() 
-                            h_data[...] = np.stack([workspace.data(model, include_auxdata=False)[idxc], [0 for i in channel_bins[channel]]], axis=-1)
-                            file[channel/'obs_data'] = h_data
-                            
+                h_data = hist.Hist.new.Regular(
+                    channel_bins[channel], 0, channel_bins[channel]
+                ).Weight()
+                h_data[...] = np.stack(
+                    [
+                        workspace.data(model, include_auxdata=False)[idxc],
+                        [0 for i in channel_bins[channel]],
+                    ],
+                    axis=-1,
+                )
+                file[channel / "obs_data"] = h_data
 
 
 def addSamples():
@@ -57,72 +67,134 @@ def addSamples():
             for idxs in range(spec["channels"][idxc]["samples"]):
                 DC.exp[channel].update(
                     {
-                        spec["channels"][idxc]["samples"][idxs]["name"]: 
-                        spec["channels"][idxc]["samples"][idxs]["data"]
+                        spec["channels"][idxc]["samples"][idxs]["name"]: spec[
+                            "channels"
+                        ][idxc]["samples"][idxs]["data"]
                     }
                 )
-        else: ##shapes
+        else:  ##shapes
             DC.hasShapes = True
             for idxs in range(spec["channels"][idxc]["samples"]):
                 DC.exp[channel].update(
-                    {
-                        spec["channels"][idxc]["samples"][idxs]["name"]: 
-                        -1
-                    }
+                    {spec["channels"][idxc]["samples"][idxs]["name"]: -1}
                 )
                 mods = [
                     spec["channels"][idxc]["samples"][idxs]["modifiers"][i]["type"]
                     for i in range(spec["channels"][idxc]["samples"][idxs]["modifiers"])
                 ]
                 if "histosys" in mods:
-                    DC.shapeMap.update({channel: {spec["channels"][idxc]["samples"][idxs]["name"]: [options.shapeFile, channel/spec["channels"][idxc]["samples"][idxs]["name"], channel/spec["channels"][idxc]["samples"][idxs]["name"]+"_$SYSTEMATIC" ]}})
+                    DC.shapeMap.update(
+                        {
+                            channel: {
+                                spec["channels"][idxc]["samples"][idxs]["name"]: [
+                                    options.shapeFile,
+                                    channel
+                                    / spec["channels"][idxc]["samples"][idxs]["name"],
+                                    channel
+                                    / spec["channels"][idxc]["samples"][idxs]["name"]
+                                    + "_$SYSTEMATIC",
+                                ]
+                            }
+                        }
+                    )
                 else:
-                    DC.shapeMap.update({channel: {spec["channels"][idxc]["samples"][idxs]["name"]: [options.shapeFile, channel/spec["channels"][idxc]["samples"][idxs]["name"]]}})
+                    DC.shapeMap.update(
+                        {
+                            channel: {
+                                spec["channels"][idxc]["samples"][idxs]["name"]: [
+                                    options.shapeFile,
+                                    channel
+                                    / spec["channels"][idxc]["samples"][idxs]["name"],
+                                ]
+                            }
+                        }
+                    )
                 if "staterror" in mods:
-                    h_data = hist.Hist.new.Regular(channel_bins[channel], 0, channel_bins[channel]).Weight()  
-                    h_data[...] = np.stack([spec["channels"][idxc]["samples"][idxs]["data"], spec["channels"][idxc]["samples"][idxs]["modifiers"][mods.index("staterror")]["data"]], axis=-1)
+                    h_data = hist.Hist.new.Regular(
+                        channel_bins[channel], 0, channel_bins[channel]
+                    ).Weight()
+                    h_data[...] = np.stack(
+                        [
+                            spec["channels"][idxc]["samples"][idxs]["data"],
+                            spec["channels"][idxc]["samples"][idxs]["modifiers"][
+                                mods.index("staterror")
+                            ]["data"],
+                        ],
+                        axis=-1,
+                    )
                 elif "shapesys" in mods:
-                    h_data = hist.Hist.new.Regular(channel_bins[channel], 0, channel_bins[channel]).Weight() 
-                    h_data[...] = np.stack([spec["channels"][idxc]["samples"][idxs]["data"], spec["channels"][idxc]["samples"][idxs]["modifiers"][mods.index("shapesys")]["data"]], axis=-1)
+                    h_data = hist.Hist.new.Regular(
+                        channel_bins[channel], 0, channel_bins[channel]
+                    ).Weight()
+                    h_data[...] = np.stack(
+                        [
+                            spec["channels"][idxc]["samples"][idxs]["data"],
+                            spec["channels"][idxc]["samples"][idxs]["modifiers"][
+                                mods.index("shapesys")
+                            ]["data"],
+                        ],
+                        axis=-1,
+                    )
                 else:
-                    h_data = hist.Hist.new.Regular(channel_bins[channel], 0, channel_bins[channel]).Weight()  
-                    h_data[...] = np.stack([spec["channels"][idxc]["samples"][idxs]["data"], [0 for i in channel_bins[channel]]], axis=-1)
+                    h_data = hist.Hist.new.Regular(
+                        channel_bins[channel], 0, channel_bins[channel]
+                    ).Weight()
+                    h_data[...] = np.stack(
+                        [
+                            spec["channels"][idxc]["samples"][idxs]["data"],
+                            [0 for i in channel_bins[channel]],
+                        ],
+                        axis=-1,
+                    )
             with uproot.update(options.shapefile) as file:
-                file[channel/spec["channels"][idxc]["samples"][idxs]["name"]] = h_data
-            
+                file[channel / spec["channels"][idxc]["samples"][idxs]["name"]] = h_data
+
+
 def addMods():
     for modifier in modifiers:
         if "normsys" in modifier[1]:
             for idxc, channel in enumerate(channels):
-                DC.systs.append((modifier[0], False, 'lnN', [], {channel : {}}))
+                DC.systs.append((modifier[0], False, "lnN", [], {channel: {}}))
                 for sample in samples:
-                    if sample in [spec["channels"][idxc]["samples"][i]["name"] for i in spec["channels"][idxc]["samples"]]: 
+                    if sample in [
+                        spec["channels"][idxc]["samples"][i]["name"]
+                        for i in spec["channels"][idxc]["samples"]
+                    ]:
                         for i in DC.systs:
-                            i[4][channel].update({sample : 0.0})           
+                            i[4][channel].update({sample: 0.0})
         if "histosys" in modifier[1]:
             for channel in channels:
-                DC.systs.append((modifier[0], False, 'shape', [], {channel: {}}))
+                DC.systs.append((modifier[0], False, "shape", [], {channel: {}}))
                 for sample in samples:
-                    if sample in [spec["channels"][idxc]["samples"][i]["name"] for i in spec["channels"][idxc]["samples"]]: 
+                    if sample in [
+                        spec["channels"][idxc]["samples"][i]["name"]
+                        for i in spec["channels"][idxc]["samples"]
+                    ]:
                         for i in DC.systs:
-                            i[4][channel].update({sample : 0.0})  
+                            i[4][channel].update({sample: 0.0})
     for idxc, channel in enumerate(channels):
-        for idxs, sample in enumerate([spec["channels"][idxc]["samples"][i]["name"] for i in spec["channels"][idxc]["samples"]]):
+        for idxs, sample in enumerate(
+            [
+                spec["channels"][idxc]["samples"][i]["name"]
+                for i in spec["channels"][idxc]["samples"]
+            ]
+        ):
             mods = []
             for i in range(spec["channels"][idxc]["samples"][idxs]["modifiers"]):
-                mods.append([spec["channels"][idxc]["samples"][idxs]["modifiers"][i]["name"], spec["channels"][idxc]["samples"][idxs]["modifiers"][i]["type"], spec["channels"][idxc]["samples"][idxs]["modifiers"][i]["data"]])
+                mods.append(
+                    [
+                        spec["channels"][idxc]["samples"][idxs]["modifiers"][i]["name"],
+                        spec["channels"][idxc]["samples"][idxs]["modifiers"][i]["type"],
+                        spec["channels"][idxc]["samples"][idxs]["modifiers"][i]["data"],
+                    ]
+                )
             for syst in DC.systs:
                 name = syst[0]
                 type = syst[2]
-                if name in mods: ##if systematic is a modifier for this sample
+                if name in mods:  ##if systematic is a modifier for this sample
                     if "lnN" in type:
-                        syst[4][channel].update({sample: mods[mods.index(name)][2]["hi"]})
+                        syst[4][channel].update(
+                            {sample: mods[mods.index(name)][2]["hi"]}
+                        )
                     if "shape" in type:
                         syst[4][channel].update({sample: 1.0})
-                        
-
-                    
-                        
-            
-                
-
