3f32518e6204247eebf80615dd63cf4f81edfa3a
Peter Ridolfi <petey.ridolfi7@gmail.com>
2022-08-17 16:33:47 +0000

    removed unused imports

--- src/pyhf_combine_converter/pyhf_convert_to_datacard.py
+++ src/pyhf_combine_converter/pyhf_convert_to_datacard.py
@@ -10,7 +10,6 @@ from hist import Hist
 import json
 from optparse import OptionParser
 try:
-    from HiggsAnalysis.CombinedLimit.DatacardParser import *
     from HiggsAnalysis.CombinedLimit.Datacard import Datacard
 except:
     print("Either the docker container has not been created properly or Combine commands have not been mounted. Please fix this and try again")
