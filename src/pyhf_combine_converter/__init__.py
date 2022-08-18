947a368cdb683f1bff37cc40bba521e3f488f80e
Matthew Feickert <matthew.feickert@cern.ch>
2022-08-18 18:07:16 -0500

    Restrict to just useful API

--- src/pyhf_combine_converter/__init__.py
+++ src/pyhf_combine_converter/__init__.py
@@ -1,8 +1,10 @@
 # Convenient access to the version number
 from pyhf_combine_converter.version import version as __version__
 
-from pyhf_combine_converter import pyhf_convert_to_datacard
-from pyhf_combine_converter import pyhf_converted_from_datacard
+from pyhf_combine_converter.pyhf_convert_to_datacard import pyhf_convert_to_datacard
+from pyhf_combine_converter.pyhf_converted_from_datacard import (
+    pyhf_converted_from_datacard,
+)
 
 __all__ = ["__version__", "pyhf_convert_to_datacard", "pyhf_converted_from_datacard"]
 
