03aaa5133de9053bd2ae7d814266b4204a254128
Matthew Feickert <matthew.feickert@cern.ch>
2022-08-18 13:44:46 -0500

    try new API

--- docs/tutorial.py
+++ docs/tutorial.py
@@ -1,7 +1,12 @@
-import pyhf_combine_converter as converter
+from pyhf_combine_converter import pyhf_converted_from_datacard
+from pyhf_combine_converter import pyhf_convert_to_datacard
 
-##convert to pyhf
-converter.pyhf_converted_from_datacard.main("converted_datacard.txt", outfile = "pyhf_workspace.json")
-##convert back to datacard
-converter.pyhf_convert_to_datacard.main("pyhf_workspace.json", outdatacard = "combine_datacard.txt", shapefile = "shapes.root")
+# convert to pyhf
+pyhf_converted_from_datacard.pyhf_convert_to_datacard(
+    "converted_datacard.txt", outfile="pyhf_workspace.json"
+)
 
+# convert back to datacard
+pyhf_convert_to_datacard.main(
+    "pyhf_workspace.json", outdatacard="combine_datacard.txt", shapefile="shapes.root"
+)
