f83fdd47d102d89a8975c12962b2e7d20e7b45da
Matthew Feickert <matthew.feickert@cern.ch>
2022-08-18 18:35:01 -0500

    Rename imports

--- src/pyhf_combine_converter/__init__.py
+++ src/pyhf_combine_converter/__init__.py
@@ -1,8 +1,7 @@
 # Convenient access to the version number
+from pyhf_combine_converter.combine_to_pyhf import pyhf_converted_from_datacard
+from pyhf_combine_converter.pyhf_to_combine import pyhf_convert_to_datacard
 from pyhf_combine_converter.version import version as __version__
-
-from pyhf_combine_converter.pyhf_convert_to_datacard import pyhf_convert_to_datacard
-from pyhf_combine_converter.pyhf_converted_from_datacard import pyhf_converted_from_datacard
 
 __all__ = ["__version__", "pyhf_convert_to_datacard", "pyhf_converted_from_datacard"]
 
