848bd1c80b0d797d92276b4972b3d899c49955d5
Matthew Feickert <matthew.feickert@cern.ch>
2022-08-18 17:53:01 -0500

    feat: Import converter modules at library import

--- src/pyhf_combine_converter/__init__.py
+++ src/pyhf_combine_converter/__init__.py
@@ -1,2 +1,11 @@
 # Convenient access to the version number
 from pyhf_combine_converter.version import version as __version__
+
+from pyhf_combine_converter import pyhf_convert_to_datacard
+from pyhf_combine_converter import pyhf_converted_from_datacard
+
+__all__ = ["pyhf_convert_to_datacard", "pyhf_converted_from_datacard", "__version__"]
+
+
+def __dir__():
+    return __all__
