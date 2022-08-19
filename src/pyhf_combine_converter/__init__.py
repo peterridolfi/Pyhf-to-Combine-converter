87c8dd6c6d4e1d5076c39e82ae99ba82932eef63
Matthew Feickert <matthew.feickert@cern.ch>
2022-08-18 21:09:27 -0500

    Also put modules in the API

--- src/pyhf_combine_converter/__init__.py
+++ src/pyhf_combine_converter/__init__.py
@@ -1,9 +1,16 @@
 # Convenient access to the version number
+from pyhf_combine_converter import combine_to_pyhf, pyhf_to_combine
 from pyhf_combine_converter.combine_to_pyhf import pyhf_converted_from_datacard
 from pyhf_combine_converter.pyhf_to_combine import pyhf_convert_to_datacard
 from pyhf_combine_converter.version import version as __version__
 
-__all__ = ["__version__", "pyhf_convert_to_datacard", "pyhf_converted_from_datacard"]
+__all__ = [
+    "__version__",
+    "combine_to_pyhf",
+    "pyhf_convert_to_datacard",
+    "pyhf_converted_from_datacard",
+    "pyhf_to_combine",
+]
 
 
 def __dir__():
