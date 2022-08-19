7a06a79841c5466dca5ff64312ce2e92204fefb3
Matthew Feickert <matthew.feickert@cern.ch>
2022-08-18 21:26:03 -0500

    Add import of pyhf

--- src/pyhf_combine_converter/__init__.py
+++ src/pyhf_combine_converter/__init__.py
@@ -1,7 +1,12 @@
-# Convenient access to the version number
+# FIXME: pyhf import is needed only to avoid UserWarnings
+# c.f. https://github.com/peterridolfi/Pyhf-to-Combine-converter/issues/30
+import pyhf
+
 from pyhf_combine_converter import combine_to_pyhf, pyhf_to_combine
 from pyhf_combine_converter.combine_to_pyhf import pyhf_converted_from_datacard
 from pyhf_combine_converter.pyhf_to_combine import pyhf_convert_to_datacard
+
+# Convenient access to the version number
 from pyhf_combine_converter.version import version as __version__
 
 __all__ = [
