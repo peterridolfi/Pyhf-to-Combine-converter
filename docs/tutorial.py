cc6ee5c271cb95d11b26a2661b99339bebc5bd6f
Peter Ridolfi <petey.ridolfi7@gmail.com>
2022-08-19 14:09:01 +0000

    add option args to tutorial

--- docs/tutorial.py
+++ docs/tutorial.py
@@ -2,7 +2,7 @@ from pyhf_combine_converter import pyhf_converted_from_datacard
 from pyhf_combine_converter import pyhf_convert_to_datacard
 
 ##convert to pyhf
-pyhf_converted_from_datacard(input_datacard = "converted_datacard.txt", outfile = "pyhf_workspace.json")
+pyhf_converted_from_datacard(input_datacard = "converted_datacard.txt", outfile = "pyhf_workspace.json", options = None)
 ##convert back to datacard
-pyhf_convert_to_datacard(workspace = "pyhf_workspace.json", outdatacard = "combine_datacard.txt", shapefile = "shapes.root")
+pyhf_convert_to_datacard(workspace = "pyhf_workspace.json", outdatacard = "combine_datacard.txt", shapefile = "shapes.root", options = None)
 
